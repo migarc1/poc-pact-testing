0399f9fe0f3adbb57991d859ff67cc343d817201
Manuel Garcia <manuel.garcia.sandoval@intel.com>
2023-07-19 12:32:49 -0600

    Upgrade pact-foundation libraries

--- publish.sh
+++ publish.sh
@@ -5,11 +5,14 @@ pact-broker publish pacts \
   --branch $GIT_BRANCH \
   --consumer-app-version $GIT_COMMIT \
   --broker-base-url ${PACT_BROKER_BASE_URL} \
-  --broker-username ${PACT_BROKER_USERNAME} \
-  --broker-password ${PACT_BROKER_PASSWORD} 
+  --broker-password ${PACT_BROKER_TOKEN}
+  #--broker-username ${PACT_BROKER_USERNAME} \
+  #--broker-password ${PACT_BROKER_PASSWORD}
 
 echo "Pact contract publishing complete!"
 echo "Head over to ${PACT_BROKER_BASE_URL}/pacts/provider/GettingStartedOrderApi/consumer/GettingStartedOrderWeb/version/$GIT_COMMIT and login with"
 echo "=> Username: ${PACT_BROKER_USERNAME}"
 echo "=> Password: ${PACT_BROKER_PASSWORD}"
 echo "to see your published contracts."
+
+read -rn1
